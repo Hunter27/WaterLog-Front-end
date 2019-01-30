aab1cade0f2289b2b85bb262e4dccb4a0d08e9d8
unknown <amathenjwa@retrorabbit.co.za>
2019-01-30 17:07:27 +0200

    Added  component jest test

--- waterlog/jest_test/src/App.test.js
+++ waterlog/jest_test/src/App.test.js
@@ -7,3 +7,13 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('invalid path should redirect to 404', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/random' ]}>
+      <App/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(LandingPage)).toHaveLength(0);
+  expect(wrapper.find(NotFoundPage)).toHaveLength(1);
+});
--- waterlog/src/App.test.js
+++ waterlog/src/App.test.js
@@ -1,9 +1,31 @@
+import 'jsdom';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import { mount,configure } from 'enzyme'
+import { MemoryRouter } from 'react-router';
+import HomeComponent from './Pages/Home';
+import AlertComponent from './Pages/Alert';
+import UsageComponent from './Pages/Usage';
+import MapComponent from './Pages/Map';
+configure({adapter: new Adapter()});
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test('invalid path should redirect to home', () => {
+  const wrapper = mount(
+    <MemoryRouter initialEntries={[ '/random' ]}>
+      <App/>
+    </MemoryRouter>
+  );
+  expect(wrapper.find(HomeComponent)).toHaveLength(1);
+  expect(wrapper.find(AlertComponent)).toHaveLength(0);
+  expect(wrapper.find(UsageComponent)).toHaveLength(0);
+  expect(wrapper.find(MapComponent)).toHaveLength(0);
 });
