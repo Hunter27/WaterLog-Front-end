3ae810f96d6a23365362117e0d499586d007fcb3
cmanamela <cmanamela@retrorabbit.co.za>
2019-01-30 14:53:20 +0200

    Added content to the home page, Addded icons on the nav dide bar, styled the navigation

--- waterlog/src/Components/Header.js
+++ waterlog/src/Components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class HeaderComponent extends Component {
   render() {
     return (
-      <div>
+      <div style={{}}>
           <h1>The Water Log Project - header</h1>
       </div>
         
---
+++ waterlog/src/Components/Tank.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../tank.scss';
+
+export default class TankComponent extends Component {
+  
+  tankName = this.props.name;
+  percentage = this.props.percentage;
+
+  render() {
+    return (
+      <div style={{width: '150px', border: '1px solid black', textAlign:'center', float: 'left', margin: '2px'}}>
+        <p>{this.tankName}</p>
+        <div 
+          style={{ border: '2px solid black', backgroundColor: 'red', width: '150px', height: '200px'}} 
+        >
+          { this.percentage} % 
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
