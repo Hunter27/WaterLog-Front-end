af3f805011de275b3a450f33337dfac4a59b9258
dadamsretrorabbit <dadams@retrorabbit.co.za>
2019-01-31 23:04:25 +0200

    Implements Offline

--- waterlog/src/index.js
+++ waterlog/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './Stylesheets/_index.scss';
 import './Stylesheets/_header.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
