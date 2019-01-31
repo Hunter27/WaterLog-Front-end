eb32f7456be36bedcdbb6b44dbf587a1188ae92c
unknown <amathenjwa@retrorabbit.co.za>
2019-01-31 13:38:14 +0200

    check if page renders

--- waterlog/jest_test/src/App.test.js
+++ waterlog/jest_test/src/App.test.js
@@ -8,12 +8,3 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-test('invalid path should redirect to 404', () => {
-  const wrapper = mount(
-    <MemoryRouter initialEntries={[ '/random' ]}>
-      <App/>
-    </MemoryRouter>
-  );
-  expect(wrapper.find(LandingPage)).toHaveLength(0);
-  expect(wrapper.find(NotFoundPage)).toHaveLength(1);
-});
