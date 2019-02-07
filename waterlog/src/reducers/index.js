3ee7a9023316caf1e26dd8f04dbc35243c38ddec
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-07 09:53:12 +0200

    Leak Costs Reducer

--- waterlog/src/reducers/index.js
+++ waterlog/src/reducers/index.js
@@ -3,10 +3,13 @@ import sensorReducer from './sensorReducer';
 import segmentsReducer from './segmentsReducer';
 import segmentEventReducer from './segmentEventsReducer';
 import segmentLeaksReducer from './segmentLeaksReducer';
-
+import leakCostsReducer from './leakCostsReducer';
+import LeakCosts from '../Components/LeakCosts';
+ 
 export default combineReducers({
     sensors: sensorReducer,
     segments: segmentsReducer,
     events: segmentEventReducer,
-    leaks: segmentLeaksReducer
+    leaks: segmentLeaksReducer,
+    costs: LeakCosts
 })
