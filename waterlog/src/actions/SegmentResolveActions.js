e409dd0725895e54fe2200e2440c1d656f390939
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-11 19:20:50 +0200

    Segment Resolve Actions

---
+++ waterlog/src/actions/SegmentResolveActions.js
@@ -0,0 +1,47 @@
+import {
+    FETCH_SEGMENTS_LEAKS_BEGIN,
+    FETCH_SEGMENTS_LEAKS_SUCCESS,
+    FETCH_SEGMENTS_LEAKS_FAILURE,
+    handleErrors
+} from './Types';
+import { Globals } from './../Globals';
+
+export const fecthSegmentsLeaksBegin = () => ({
+    type: FETCH_SEGMENTS_LEAKS_BEGIN
+});
+
+export const fecthSegmentsLeaksSuccess = (leaks) => ({
+    type: FETCH_SEGMENTS_LEAKS_SUCCESS,
+    payload: { leaks }
+});
+
+export const fecthSegmentsLeaksFailure = (error) => ({
+    type: FETCH_SEGMENTS_LEAKS_FAILURE,
+    payload: { error }
+});
+
+//TODO: Resolve promise hell
+export const fetchSegmentsLeaks = () => (dispatch) => {
+    dispatch(fecthSegmentsLeaksBegin());
+    return fetch(`${Globals.API_URL}/api/segmentleaks`)
+        .then(handleErrors)
+        .then((res) => res.json())
+        .then((leaks) => {
+            var count = 0;
+            leaks.map((leak) =>
+                fetch(`${Globals.API_URL}/api/segmentleaks/resolve/${leak.id}`)
+                    .then((res) => res.json())
+                    .then((resolve) => {
+                        leaks[count].resolve = resolve;
+                        count = count + 1;
+                        if (count === leaks.length) {
+                            dispatch(fecthSegmentsLeaksSuccess(leaks));
+                        }
+                    })
+            );
+            return leaks;
+        })
+        .catch((error) => {
+            dispatch(fecthSegmentsLeaksFailure(error));
+        });
+};
