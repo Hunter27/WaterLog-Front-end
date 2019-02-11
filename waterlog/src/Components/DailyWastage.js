5849f993dbcdf976c0afe28c4a99c72f3bd6fc98
cmanamela <cmanamela@retrorabbit.co.za>
2019-02-11 18:34:20 +0200

    Moved styles to separate files and removed comments on the code

--- waterlog/src/Components/DailyWastage.js
+++ waterlog/src/Components/DailyWastage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Loader from './Loader';
 import { Line, defaults } from 'react-chartjs-2';
 
 const DailyWastageComponent = (props) => {
@@ -16,14 +15,13 @@ const DailyWastageComponent = (props) => {
       {
         label: 'Temperature',
         data: [10,19,27,23,22,4,100,25,23,24,20,19],
-        fill: true,          // Don't fill area under the line
-        borderColor: 'blue'  // Line color
+        fill: true,         
+        borderColor: 'blue'  
       }
     ]
   }
   var options = {
     maintainAspectRatio: true,
-    //scales: { xAxes: [{ display: false, }], yAxes: [{ display: false, }], }
   }
     defaults.global.legend.display = false;
     return (
