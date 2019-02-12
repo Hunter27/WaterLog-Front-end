6b2bad2d7287216f62c273ddcc24509c8540f0ad
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-12 18:42:09 +0200

    Updated index action and types

--- waterlog/src/actions/SegmentEventsActions.js
+++ waterlog/src/actions/SegmentEventsActions.js
@@ -1,37 +1,36 @@
 import {
-    FETCH_SEGMENTS_EVENTS_BEGIN,
-    FETCH_SEGMENTS_EVENTS_SUCCESS,
-    FETCH_SEGMENTS_EVENTS_FAILURE,
-    handleErrors
-} from './Types';
-import { Globals } from './../Globals';
+  FETCH_SEGMENT_EVENTS_BEGIN,
+  FETCH_SEGMENT_EVENTS_SUCCESS,
+  FETCH_SEGMENT_EVENTS_FAILURE,
+  handleErrors
+} from "./Types";
+import { Globals } from "./../Globals";
 
-export const fecthSegmentsResolvesBegin = () => ({
-    type: FETCH_SEGMENTS_EVENTS_BEGIN
+export const fecthSegmentEventsBegin = () => ({
+  type: FETCH_SEGMENT_EVENTS_BEGIN
 });
 
-export const fecthSegmentsResolvesSuccess = (leaksResolves) => ({
-    type: FETCH_SEGMENTS_EVENTS_SUCCESS,
-    payload: { leaksResolves }
+export const fecthSegmentEventsSuccess = leaksResolves => ({
+  type: FETCH_SEGMENT_EVENTS_SUCCESS,
+  payload: { leaksResolves }
 });
 
-export const fecthSegmentsResolvesFailure = (error) => ({
-    type: FETCH_SEGMENTS_EVENTS_FAILURE,
-    payload: { error },
-    loading: false
+export const fecthSegmentEventsFailure = error => ({
+  type: FETCH_SEGMENT_EVENTS_FAILURE,
+  payload: { error },
+  loading: false
 });
 
 //TODO: Resolve promise hell
-export const fetchSegmentsLeaksResolve = id => (dispatch) => {
-    dispatch(fecthSegmentsResolvesBegin());
-    return fetch(`${Globals.API_URL}/api/segmentevents/${id}`)
-        .then(handleErrors)
-        .then((res) => res.json())
-        .then((leaksResolves) => {
-            dispatch(fecthSegmentsResolvesSuccess(leaksResolves));
-        })
-        .catch((error) => {
-            dispatch(fecthSegmentsResolvesFailure(error));
-        });
+export const fetchSegmentsLeaksResolve = id => dispatch => {
+  dispatch(fecthSegmentEventsBegin());
+  return fetch(`${Globals.API_URL}/api/segmentevents/${id}`)
+    .then(handleErrors)
+    .then(res => res.json())
+    .then(leaksResolves => {
+      dispatch(fecthSegmentEventsSuccess(leaksResolves));
+    })
+    .catch(error => {
+      dispatch(fecthSegmentEventsFailure(error));
+    });
 };
-
--- waterlog/src/reducers/index.js
+++ waterlog/src/reducers/index.js
@@ -4,10 +4,12 @@ import SegmentLeaksReducer from './SegmentLeaksReducer';
 import SegmentLeaksByIdReducer from './SegmentLeaksByIdReducer';
 import LeakLitresReducer from './LeakLitresReducer';
 import LeaksResolvedReducer from './LeaksResolvedReducer';
+import SegmentEventsReducer from './SegmentEventsReducer';
 
 export default combineReducers({
 	leaks: SegmentLeaksReducer,
 	leak: SegmentLeaksByIdReducer,
 	litres: LeakLitresReducer,
-	leaksResolves: LeaksResolvedReducer 
+	leaksResolves: LeaksResolvedReducer,
+    events: SegmentEventsReducer
 });
