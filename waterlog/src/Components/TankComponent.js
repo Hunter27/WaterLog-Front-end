20df4d06310adf8af04e37abe24540656e575033
unknown <amathenjwa@retrorabbit.co.za>
2019-02-13 13:52:49 +0200

    Done Displaying the top part of the tank level page

--- waterlog/src/Components/TankComponent.js
+++ waterlog/src/Components/TankComponent.js
@@ -9,6 +9,46 @@ export class TankComponent extends Component {
   }
   render() {
     const { error, loading, levels } = this.props;
+    const getTankImage = (percent) => {
+      if (percent === 100) {
+        return <img src="/images/ICONS/100_tank.png"
+          width="150"
+          height="150"
+          alt="50% tank" />
+      }
+      else if (percent === 50) {
+        return <img src="/images/ICONS/50_tank.png"
+          width="150"
+          height="150"
+          alt="50% tank" />
+      }
+      else if (percent === 0) {
+        return <img src="/images/ICONS/0_tank.png"
+          width="150"
+          height="150"
+          alt="50% tank" />
+      }
+      else if (percent >= 1 && percent <= 40) {
+        return (
+          <div>
+            <img src="/images/ICONS/tank-yellow.png"
+              width="150"
+              height="150"
+              alt="50% tank" />
+            <p id="percentage">{percent}%</p>
+          </div>
+        )
+      }
+      else if (percent >= 41 && percent <= 79) {
+        return (<div> <img src="/images/ICONS/tank-orange.png"
+          width="150"
+          height="150"
+          alt="50% tank" />
+          <p  id="percentage">{percent}%</p>
+        </div>
+        )
+      }
+    }
 
     if (error) {
       return <div>Error! {error.message}</div>;
@@ -16,25 +56,18 @@ export class TankComponent extends Component {
     if (loading) {
       return <div>Loading...</div>;
     }
-    var level = levels[0]
+    var level = levels[3]
     return (
-
       <div className={"tankComponent"}>
         <h3>{"Tank " + level.tank_Id}</h3>
-        
-
-        <img src="/images/ICONS/100_tank.png"
-          width="150"
-          height="150"
-          alt="50% tank" />
-
+        {getTankImage(level.percentage)}
         <h5>{level.level_Status}</h5>
         <h5> {level.instruction}</h5>
       </div>
-
     );
   }
 }
+
 TankComponent.propTypes = {
   fetchTankLevels: PropTypes.func.isRequired,
   levels: PropTypes.array.isRequired
