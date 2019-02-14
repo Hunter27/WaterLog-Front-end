95a7b75937bdf153defb728e40901df7188d8545
cmanamela <cmanamela@retrorabbit.co.za>
2019-02-14 16:03:25 +0200

    Added graphs on the usage page

--- waterlog/src/Pages/Usage.js
+++ waterlog/src/Pages/Usage.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
+import UsageCostComponent from './../Components/Usage-cost';
 
 class UsageComponent extends Component {
   render() {
-    return <div>Usage page</div>;
+    return <div>
+      Usage page
+      <UsageCostComponent />
+      </div>;
   }
 }
 export default UsageComponent;
