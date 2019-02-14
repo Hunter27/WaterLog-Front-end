d81e59a7cd7deaf3e18d7b214586cfef011c9798
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-14 13:51:24 +0200

    Update resolve function

--- waterlog/src/Components/BtnResolve.js
+++ waterlog/src/Components/BtnResolve.js
@@ -1,22 +1,25 @@
- import React, { Component } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchSegmentsLeaksResolve } from "../actions/SegmentResolveActions";
- 
+import { Globals } from './../Globals';
 
 class BtnResolve extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
       text: "Resolve Issue"
     };
   }
+
   componentDidMount() {
     this.props.fetchSegmentsLeaksResolve(this.props.id);
   }
+
   render() {
     const { error, loading, leaksResolves } = this.props;
-
+    console.log({ leaksResolves });
     if (error) {
       return <div>Error! {error.message}</div>;
     }
@@ -24,6 +27,7 @@ class BtnResolve extends Component {
       return <div>Loading...</div>;
     }
     return (
+
       <div>
         <h4>{this.state.text}</h4>
         <button
@@ -39,15 +43,20 @@ class BtnResolve extends Component {
 
   resolve(leaksObject) {
     leaksObject = this.props.leaksResolves;
-    if (leaksObject.resolvedStatus === "unresolved") {
-      this.setState({
-        text: "Leak not resolved"
-      });
-    } else {
-      this.setState({
-        text: "Issue resolved "
-      });
-    }
+    leaksObject.resolvedStatus = "resolved";
+    return fetch(`${Globals.API_URL}/api/segmentleaks/${leaksObject.id}`, {
+      method: "PUT",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      referrer: "no-referrer",
+      body: JSON.stringify(leaksObject),
+    })
+      .then(response => response.json());
   }
 }
 
@@ -64,4 +73,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { fetchSegmentsLeaksResolve }
-)(BtnResolve);
+)(BtnResolve); 
