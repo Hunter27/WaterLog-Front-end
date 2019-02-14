c94f0873010746acbc00b5b1f8c56149ab869ca8
GiftLanga <glanga@retrorabbit.co.za>
2019-02-14 15:11:52 +0200

    added map on the home page

--- waterlog/src/Components/AlertsMap.js
+++ waterlog/src/Components/AlertsMap.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Loader from './Loader';
+import MapComponent from './Map';
 
 const AlertsMap = (props) => {
-    return (
-      <div className="alerts-map">
-        <Loader />
-      </div>
-    )
-}
+	const { error, loading, sensorLocations } = props;
+
+	if (error) {
+		return <div>Error! {error.message}</div>;
+	}
+	if (loading) {
+		return (
+			<div className="alerts-map">
+				<Loader />
+			</div>
+		);
+	}
+
+	return <MapComponent />
+};
 export default AlertsMap;
--- waterlog/src/Pages/Home.js
+++ waterlog/src/Pages/Home.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Wastage from './../Components/Wastage';
 import AlertsMap from './../Components/AlertsMap';
 import TankLevels from './../Components/TankLevels';
 
 class HomeComponent extends Component {
-  render() {
-    return (
-      <div>
-        <p className="home-text">Alerts Map</p>
-      <AlertsMap />
-      <Wastage />
-      <p className="home-text">Tank Levels</p>
-      <TankLevels />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<p className="home-text">Alerts Map</p>
+				<AlertsMap />
+				<Wastage />
+				<p className="home-text">Tank Levels</p>
+				<TankLevels />
+			</div>
+		);
+	}
 }
 export default HomeComponent;
