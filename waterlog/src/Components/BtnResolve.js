1fad85490c32c8e963f33cea08ed7ad21c487a8c
MsweliP <tmsweli@retrorabbit.co.za>
2019-02-27 10:11:11 +0200

    Indentation

--- waterlog/src/Components/BtnResolve.js
+++ waterlog/src/Components/BtnResolve.js
@@ -5,18 +5,18 @@ import { fetchSegmentsLeaksResolve } from '../actions/SegmentResolveActions';
 import { Globals } from './../Globals';
 
 class BtnResolve extends Component {
-  constructor(props) {
+	constructor(props) {
 		super(props);
 		this.state = {
 			text: "Resolve Issue"
 		};
-  }
+	}
 
-  componentDidMount() {
-    this.props.fetchSegmentsLeaksResolve(this.props.id);
-  }
+	componentDidMount() {
+		this.props.fetchSegmentsLeaksResolve(this.props.id);
+	}
 
-  	render() {
+	render() {
 		const { error, loading, leaksResolves } = this.props;
 		if (error) {
 			return <div>Error! {error.message}</div>;
@@ -25,19 +25,19 @@ class BtnResolve extends Component {
 			return <div>Loading...</div>;
 		}
 		return (
-		<div>
-			<h4>{this.state.text}</h4>
-			<button
-				onClick={() => this.resolve(leaksResolves)}
-				className="BtnResolve"
-			>
-				RESOLVE
+			<div>
+				<h4>{this.state.text}</h4>
+				<button
+					onClick={() => this.resolve(leaksResolves)}
+					className="BtnResolve"
+				>
+					RESOLVE
 			</button>
-		</div>
-    );
-  }
+			</div>
+		);
+	}
 
-  resolve(leaksObject) {
+	resolve(leaksObject) {
 		leaksObject = this.props.leaksResolves;
 		leaksObject.resolvedStatus = "resolved";
 		return fetch(`${Globals.API_URL}/api/segmentleaks/${leaksObject.id}`, {
@@ -51,13 +51,13 @@ class BtnResolve extends Component {
 			redirect: "follow",
 			referrer: "no-referrer",
 			body: JSON.stringify(leaksObject),
-    })
-    .then(response => response.json());
-  }
+		})
+			.then(response => response.json());
+	}
 }
 
 BtnResolve.propTypes = {
-  	fetchSegmentsLeaksResolve: PropTypes.func.isRequired
+	fetchSegmentsLeaksResolve: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
