81b9f6a3056a7c49654ccffd0c014ba02c0d2a8d
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-27 09:07:12 +0200

    Added SignalR middleware and updated redux store

--- waterlog/src/Components/DailyCostsReports.js
+++ waterlog/src/Components/DailyCostsReports.js
@@ -5,14 +5,15 @@ const DailyCostsReports = (props) => {
   let labelX = props.props.dailyCost.dataPoints.map(a => (new Date(a.x).getHours() + ":00"));
   let dataY = props.props.dailyCost.dataPoints.map(a => Math.round(a.y)); 
   const {yIntercept, slope} = props.props.forecastDaily[0];
-  let startDate = new Date(props.props.dailyCost.dataPoints[0].x).getTime();
-  let lastDate = new Date(props.props.dailyCost.dataPoints[labelX.length-1].x).getTime();
+  const today = new Date(); 
+  let startDate = new Date(today.getYear(), today.getMonth(),today.getDay(), 0, 0, 0, 0).getTime();
+  let lastDate = new Date(today.getYear(), today.getMonth(),today.getDay(), 23, 59, 0, 0).getTime();
   const startX = Math.floor( startDate/ 1000); 
   const lastX = Math.floor(lastDate/ 1000); 
 
   let forecast = [];
   for(let i = startX; i <= lastX; i = i + 3600){
-    forecast.push(slope*i + yIntercept);
+    forecast.push(null);
   }
  
   let data = {
@@ -50,8 +51,7 @@ const DailyCostsReports = (props) => {
         ticks: {
           major: {
             fontStyle: 'bold',
-            fontColor: 'rgba(255,0,0,1)',
-            maxTicksLimit: 24
+            fontColor: 'rgba(255,0,0,1)', 
           }
         },
         gridLines: {
--- waterlog/src/ConfigRedux/store/configureStore.js
+++ waterlog/src/ConfigRedux/store/configureStore.js
@@ -3,10 +3,11 @@ import thunkMiddleware from 'redux-thunk';
 import monitorReducersEnhancer from '../enhancers/monitorReducer';
 import loggerMiddleware from '../middleware/logger';
 import rootReducer from '../../../src/reducers/rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import signalRMiddleware from '../middleware/signalRMiddleware';
 
 export default function configureStore(preloadedState) {
-    const middlewares = [loggerMiddleware, thunkMiddleware]
+    const middlewares = [loggerMiddleware, thunkMiddleware, signalRMiddleware]
     const middlewareEnhancer = applyMiddleware(...middlewares)
     const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
     const composedEnhancers = composeWithDevTools(...enhancers)
