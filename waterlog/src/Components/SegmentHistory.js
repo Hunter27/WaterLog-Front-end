978ea10ce1412fa63edb517e7f71c7e46be2c561
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-28 15:55:39 +0200

    Fixed indentation and tabs

--- waterlog/src/Components/SegmentHistory.js
+++ waterlog/src/Components/SegmentHistory.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import HistoryTable from './HistoryTable';
+import React, { Component } from "react";
+import HistoryTable from "./HistoryTable";
 
 class SegmentHistory extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            id: this.props.match.params.id
-        }
-    }
-    render() {
-        return ( 
-            <div> 
-                <p className="home-text">Segment { this.state.id }</p>
-				<p className="home-text">Alert History</p>
-                <HistoryTable id={this.state.id} />
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: this.props.match.params.id
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p className="home-text">Segment {this.state.id}</p>
+        <p className="home-text">Alert History</p>
+        <HistoryTable id={this.state.id} />
+      </div>
+    );
+  }
 }
 export default SegmentHistory;
