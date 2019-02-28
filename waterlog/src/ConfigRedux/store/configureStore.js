8edc698aa7aae433ec9c48f7eee0c7094dd104f0
lisaQ04 <lquluba@retrorabbit.co.za>
2019-02-28 16:02:32 +0200

    Updated config store

--- waterlog/src/ConfigRedux/store/configureStore.js
+++ waterlog/src/ConfigRedux/store/configureStore.js
@@ -3,11 +3,10 @@ import thunkMiddleware from 'redux-thunk';
 import monitorReducersEnhancer from '../enhancers/monitorReducer';
 import loggerMiddleware from '../middleware/logger';
 import rootReducer from '../../../src/reducers/rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import signalRMiddleware from '../middleware/signalRMiddleware';
+import { composeWithDevTools } from 'redux-devtools-extension'; 
 
 export default function configureStore(preloadedState) {
-    const middlewares = [loggerMiddleware, thunkMiddleware, signalRMiddleware]
+    const middlewares = [loggerMiddleware, thunkMiddleware]
     const middlewareEnhancer = applyMiddleware(...middlewares)
     const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
     const composedEnhancers = composeWithDevTools(...enhancers)
