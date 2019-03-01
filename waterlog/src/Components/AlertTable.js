53a563529571de4db37b8de7037009a91a28b3b3
cmanamela <cmanamela@retrorabbit.co.za>
2019-03-01 11:48:49 +0200

    Formated the code

--- waterlog/src/Components/AlertTable.js
+++ waterlog/src/Components/AlertTable.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchAlerts } from '../actions/AlertsAction';
+import { fetchFilteredAlerts } from '../actions/FilterAction';
 import Loader from './Loader';
 import Error404 from './Error404';
 import { formatDate, getStatusIcon } from './../utils';
@@ -15,8 +16,17 @@ class AlertTableComponent extends Component {
 		};
 	}
 	componentDidMount() {
+		const request={ "Segment":"0",
+		"SensorType":"0",
+		"SensorId":"0",
+		"Severity":"2"};
+		this.props.fetchFilteredAlerts(request);
+
+		
+
 		document.addEventListener('scroll', this.trackScrolling);
-		if (this.props.alerts.length == 0) this.props.fetchAlerts(this.props.page);
+		if (this.props.alerts.length == 0) 
+			this.props.fetchAlerts(this.props.page);
 	}
 	nextPage = () => {
 		this.props.fetchAlerts(this.props.page);
@@ -33,7 +43,9 @@ class AlertTableComponent extends Component {
 		}
 
 		const sortAlerts = () => {
-			this.state.ascending ? alerts.sort((a, b) => b.status - a.status) : alerts.sort((a, b) => a.status - b.status);
+			this.state.ascending ? 
+				alerts.sort((a, b) => b.status - a.status) : 
+				alerts.sort((a, b) => a.status - b.status);
 			this.setState({ ascending: !this.state.ascending });
 		};
 
@@ -47,22 +59,29 @@ class AlertTableComponent extends Component {
 						alt="ascending_descending icon"
 						onClick={() => sortAlerts()}
 					/>
-					<img className="alert-img"  src="images/filter_icon.png" alt="filter icon"/>
+					<img className="alert-img"  
+						src="images/filter_icon.png" 
+						alt="filter icon"/>
 				</div>
 				<table className="alerts-table">
 					<tbody>
 						{alerts.map((alert, index) => (
 							<tr
 								key={index}
-								className={`table-row ${parseInt(alert.status) == 2 ? 'table-row-unresolved' : ''}`}
+								className={`table-row ${parseInt(alert.status) == 2 ? 
+									'table-row-unresolved' : ''}`}
 								onClick={() => (window.location.href = `alert/${alert.entityName}/${alert.entityId}/${alert.date}`)}
 							>
 								<td className="event-date">{formatDate(alert.date)}</td>
 								<td>{`${alert.entityName.toUpperCase()} ${alert.entityId} ${alert.entityType.toUpperCase()}`}</td>
-								<td>{alert.entityName === 'Segment' ? `R${parseInt(alert.cost).toFixed(2)}/hr` : ''}</td>
+								<td>{alert.entityName === 'Segment' ? 
+										`R${parseInt(alert.cost).toFixed(2)}/hr` : 
+										''}
+								</td>
 								<td>
 									{alert.entityName === 'Segment' ? (
-										<img alt="severity indicator" src={getStatusIcon(alert.severity)} className="severity-indicator" />
+										<img alt="severity indicator" 
+											src={getStatusIcon(alert.severity)} className="severity-indicator" />
 									) : (
 										''
 									)}
@@ -91,7 +110,8 @@ const mapStateToProps = (state) => ({
 	f_alerts: state.filteredAlerts.items,
 	loading: state.alerts.loading,
 	page: state.alerts.page,
-	error: state.alerts.error
+	error: state.alerts.error,
+
 });
 
-export default connect(mapStateToProps, { fetchAlerts })(AlertTableComponent);
+export default connect(mapStateToProps, { fetchAlerts, fetchFilteredAlerts })(AlertTableComponent);
