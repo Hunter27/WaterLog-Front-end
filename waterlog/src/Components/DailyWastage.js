9bf7c313782d58d9729821fdcb7bf1186f4b0618
lisaQ04 <lquluba@retrorabbit.co.za>
2019-03-04 14:58:59 +0200

    Daily Wastage report update

--- waterlog/src/Components/DailyWastage.js
+++ waterlog/src/Components/DailyWastage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Line, defaults } from 'react-chartjs-2';
 import {Globals } from './../Globals';
+import {mapDataTime} from '../utils';
 
 const DailyWastageComponent = (props) => { 
   var labelX = props.props.placeholder.map(a => (new Date(a).getHours() + ":00"));
-  var dataY = props.props.dailyWaste.dataPoints.map(a => Math.round(a.y));
+  var myData = props.props.dailyWaste.dataPoints;
+  let dataY = mapDataTime(myData); 
   var sum = dataY.reduce((a, b) => a + b, 0); 
   var data = {
     labels: labelX,
