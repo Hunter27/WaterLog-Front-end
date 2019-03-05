f16eec465ac75ce4f1747a9fb51b2a9f41bb3284
lisaQ04 <lquluba@retrorabbit.co.za>
2019-03-05 09:26:46 +0200

    Àdded actions for filtered alerts

--- waterlog/src/actions/AlertsAction.js
+++ waterlog/src/actions/AlertsAction.js
@@ -2,8 +2,25 @@ import {
 	FETCH_ALERTS_BEGIN,
 	FETCH_ALERTS_SUCCESS,
 	FETCH_ALERTS_FAILURE,
+	FETCH_FILTERED_ALERTS_BEGIN,
+	FETCH_FILTERED_ALERTS_SUCCESS,
+	FETCH_FILTERED_ALERTS_FAILURE,
 	handleErrors
-} from './Types';
+} from './Types'; 
+
+export const fetchFilteredAlertsBegin =()=>({
+	type: FETCH_FILTERED_ALERTS_BEGIN
+});
+
+export const fetchFilteredAlertsSuccess = alerts =>({
+	type: FETCH_FILTERED_ALERTS_SUCCESS,
+	payload:{alerts} 
+});
+
+export const fetchFilteredAlertsFailure = error =>({
+	type: FETCH_FILTERED_ALERTS_FAILURE,
+	payload: {error}
+})
 
 export const fetchAlertsBegin = () => ({
 	type: FETCH_ALERTS_BEGIN
@@ -14,7 +31,6 @@ export const fetchAlertsSuccess = alerts => ({
 	payload: {
 		alerts,
 		total: alerts.filter(alert => alert.status === 2).length
-
 	}
 });
 
@@ -34,4 +50,15 @@ export const fetchAlerts = (id) => dispatch => {
 			dispatch(fetchAlertsSuccess(alerts));
 		})
 		.catch(error => dispatch(fetchAlertsFailure(error)));
+};
+
+export const fetchFilteredAlerts =() => dispatch =>{
+	dispatch(fetchFilteredAlertsBegin());
+	fetch(`${process.env.REACT_APP_API_URL}api/segmentevents/getalertsfilter?segment=5&sensorId=1&sensortype=2&severity=3`)
+	.then(handleErrors)
+	.then(res => res.json())
+	.then(alerts=>{
+		dispatch(fetchAlertsSuccess(alerts));
+	})
+	.catch(error => dispatch(fetchFilteredAlertsFailure(error)))
 };
