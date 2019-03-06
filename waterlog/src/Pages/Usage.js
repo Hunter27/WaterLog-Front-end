c3512ce7438c1aeef501d7a81a7126067f5f894a
cmanamela <cmanamela@retrorabbit.co.za>
2019-03-06 08:07:14 +0200

    Deleted unused file and minor fixes

--- waterlog/src/Pages/Usage.js
+++ waterlog/src/Pages/Usage.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import Usage from "./../Components/Usage";
-import TryCedric from "./../Components/TryCedric";
 class UsageComponent extends Component {
   render() {
     return (
-      <div> 
-        <TryCedric segment={5} sensorId={1} sensortype={2} severity={3}/>
-        <Usage/>
-      </div>
+      <Usage/>
     );
   }
 }
