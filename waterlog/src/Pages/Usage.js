4e7618da91dc93758140a6a29bc7fb42ee012344
lisaQ04 <lquluba@retrorabbit.co.za>
2019-03-06 13:49:04 +0200

    Removed dummy component

--- waterlog/src/Pages/Usage.js
+++ waterlog/src/Pages/Usage.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import Usage from "./../Components/Usage";
-import TryCedric from "./../Components/TryCedric";
+import Usage from "./../Components/Usage"; 
 class UsageComponent extends Component {
   render() {
     return (
-      <div> 
-        <TryCedric segment={5} sensorId={1} sensortype={2} severity={3}/>
+      <div>  
         <Usage/>
       </div>
     );
