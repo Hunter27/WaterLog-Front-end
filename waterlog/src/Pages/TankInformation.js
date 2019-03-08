fe1fc58ad0bb848ec1218da5803eeb0c98c8984e
GiftLanga <glanga@retrorabbit.co.za>
2019-03-08 12:02:37 +0200

    import images to variables

--- waterlog/src/Pages/TankInformation.js
+++ waterlog/src/Pages/TankInformation.js
@@ -11,68 +11,72 @@ import Error404 from './Error404Page';
 import Loader from './../Components/Loader';
 import Map from './../Components/Map';
 import { Globals } from './../Globals';
+import back_button from "./../images/back_button.png"
+import map_expand from './../images/map_expand.png'
+import map_close from './../images/map_close.png'
+
 
 class TankInformation extends Component {
   constructor(props) {
     super(props);
     this.state = {
-			id: this.props.match.params.id,
-			pumpOn: true,
-			mapExpanded: false
+      id: this.props.match.params.id,
+      pumpOn: true,
+      mapExpanded: false
     };
-	}
-	
-	componentWillMount() {
-		this.props.fetchTankLevelById(this.state.id);
-	}
+  }
+
+  componentWillMount() {
+    this.props.fetchTankLevelById(this.state.id);
+  }
 
   handleMapExpand() {
     this.setState({
       mapExpanded: !this.state.mapExpanded
     });
-	}
-	
-	segmentMap = (id) => (
-		<div className="segment-map">
-			<Map type={Globals.COMPONENT_TYPES.TANK} id={id} />
-		</div>
-	);
+  }
 
-	render() {
-		const { error, loading, level } = this.props;
-		if (error) {
-			return <Error404 />;
-		}
-		if (loading) {
-			return <Loader />;
-		}
+  segmentMap = (id) => (
+    <div className="segment-map">
+      <Map type={Globals.COMPONENT_TYPES.TANK} id={id} />
+    </div>
+  );
 
-		const { percentageLevel, optimalLevel } = level;
-		
+  render() {
+    const { error, loading, level } = this.props;
+    if (error) {
+      return <Error404 />;
+    }
+    if (loading) {
+      return <Loader />;
+    }
+
+    const { percentageLevel, optimalLevel } = level;
+
     return (
       <div className="tank-info">
-				<img className = "back-icon" 
-					src = 'images/back_button.png'
-					alt = "backButton"
-					onClick={() => this.props.history.goBack()}
-				/>
-				<Tank tank={level} />
-				{percentageLevel === optimalLevel ? <p className="optimal">optimal level</p> : null}
-				<p className="p-status">pump is {this.state.pumpOn ? 'on' : 'off'}</p>
-				{((parseInt(percentageLevel) >= parseInt(optimalLevel)) && this.state.pumpOn)
-					? <p className="p-info">the tank is being overfilled</p>
-					: null}
-				{percentageLevel <= 5 ? <p className= "t-empty" >the tank is empty</p> : null}
-				<PumpButton id={this.state.id} />
-				<img
-					id="map-toggle"
-					src={this.state.mapExpanded === false ?
-						'images/map_expand.png' :
-						'images/map_close.png'}
-					alt="segment-map"
-					onClick={() => this.handleMapExpand()}
-				/>
-				{this.state.mapExpanded ? this.segmentMap(this.state.id) : null}
+        <img className="back-icon"
+          src={back_button}
+          alt="backButton"
+          onClick={() => this.props.history.goBack()}
+        />
+        <Tank tank={level} />
+        {percentageLevel === optimalLevel ? <p className="optimal">optimal level</p> : null}
+        <p className="p-status">pump is {this.state.pumpOn ? 'on' : 'off'}</p>
+        {((parseInt(percentageLevel) >= parseInt(optimalLevel)) && this.state.pumpOn)
+          ? <p className="p-info">the tank is being overfilled</p>
+          : null}
+        {percentageLevel <= 5 ? <p className="t-empty" >the tank is empty</p> : null}
+        <PumpButton id={this.state.id} />
+        <img
+          id="map-toggle"
+          src={this.state.mapExpanded === false ?
+            map_expand :
+            map_close}
+          alt="segment-map"
+          onClick={() => this.handleMapExpand()}
+        />
+        {this.state.mapExpanded ? this.segmentMap(this.state.id) : null}
         <GraphLine className="UsageTnk" id={this.state.id} />
       </div>
     );
@@ -80,21 +84,21 @@ class TankInformation extends Component {
 }
 
 TankInformation.propTypes = {
-	fetchTankLevelById: PropTypes.func.isRequired,
-	level: PropTypes.array.isRequired,
-	fetchPumps: PropTypes.func.isRequired,
-	pump: PropTypes.array.isRequired,
+  fetchTankLevelById: PropTypes.func.isRequired,
+  level: PropTypes.array.isRequired,
+  fetchPumps: PropTypes.func.isRequired,
+  pump: PropTypes.array.isRequired,
 };
 
 const mapStateToProps = state => ({
-	level: state.level.item,
-	tankLoading: state.level.loading,
-	tankError: state.level.errors,
-	pump: state.pump.item,
-	pumpError: state.pump.error,
-	pumpLoading: state.pump.loading
+  level: state.level.item,
+  tankLoading: state.level.loading,
+  tankError: state.level.errors,
+  pump: state.pump.item,
+  pumpError: state.pump.error,
+  pumpLoading: state.pump.loading
 });
 export default connect(
-	mapStateToProps,
-	{ fetchPumps, fetchTankLevelById }
+  mapStateToProps,
+  { fetchPumps, fetchTankLevelById }
 )(TankInformation);
