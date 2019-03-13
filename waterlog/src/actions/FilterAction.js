02198dcddd090adbd1dd50e920af37b2d353ca30
GiftLanga <glanga@retrorabbit.co.za>
2019-03-13 16:46:52 +0200

    changed to Post

--- waterlog/src/actions/FilterAction.js
+++ waterlog/src/actions/FilterAction.js
@@ -1,40 +1,43 @@
 import {
-	FETCH_FILTERED_ALERTS_BEGIN,
-	FETCH_FILTERED_ALERTS_SUCCESS,
-	FETCH_FILTERED_ALERTS_FAILURE,
-	handleErrors
+  FETCH_FILTERED_ALERTS_BEGIN,
+  FETCH_FILTERED_ALERTS_SUCCESS,
+  FETCH_FILTERED_ALERTS_FAILURE,
+  handleErrors
 } from './Types';
 
 export const fetchFilteredAlertsBegin = () => ({
-	type: FETCH_FILTERED_ALERTS_BEGIN
+  type: FETCH_FILTERED_ALERTS_BEGIN
 });
 
 export const fetchFilteredAlertsSuccess = (filteredAlerts) => ({
-	type: FETCH_FILTERED_ALERTS_SUCCESS,
-	payload: {
-		filteredAlerts
-	}
+  type: FETCH_FILTERED_ALERTS_SUCCESS,
+  payload: {
+    filteredAlerts
+  }
 });
 
 export const fetchFilteredAlertsFailure = (error) => ({
-	type: FETCH_FILTERED_ALERTS_FAILURE,
-	payload: {
-		error
-	}
+  type: FETCH_FILTERED_ALERTS_FAILURE,
+  payload: {
+    error
+  }
 });
 
 export const fetchFilteredAlerts = (criteria) => (dispatch) => {
-	dispatch(fetchFilteredAlertsBegin());
-	fetch(`${process.env.REACT_APP_API_URL}/api/segmentevents/getalertsfilter`, {
-		method: 'GET',
-		body: JSON.stringify(criteria)
-	})
-		.then(handleErrors)
-		.then((res) => res.json())
-		.then((_alerts) => {
-			dispatch(fetchFilteredAlertsSuccess(_alerts));
-		})
-		.catch((error) => {
-			dispatch(fetchFilteredAlertsFailure(error));
-		})
+  dispatch(fetchFilteredAlertsBegin());
+  fetch(`${process.env.REACT_APP_API_URL}/api/segmentevents/getalertsfilter`, {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(criteria)
+  })
+    .then(handleErrors)
+    .then((res) => res.json())
+    .then((_alerts) => {
+      dispatch(fetchFilteredAlertsSuccess(_alerts));
+    })
+    .catch((error) => {
+      dispatch(fetchFilteredAlertsFailure(error));
+    })
 };
